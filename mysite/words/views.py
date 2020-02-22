bae280c72b5d58035f0963fc88fa6d7ec997cbe2
Raul <r_mp22@hotmail.com>
2020-02-22 17:46:28 -0600

    templates

--- mysite/words/views.py
+++ mysite/words/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Word
 
 def index(request):
-    return HttpResponse("Words is working")
+    consulta = Word.objects.all()
+    context = {
+        'context':consulta
+    }
+    return render(request,'../templates/indez.html',context)
